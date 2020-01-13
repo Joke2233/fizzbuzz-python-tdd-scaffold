322a514397768b4b83d1d6558a09aed1a799eabc
unknown <wanshuxiao@gmail.com>
2020-01-13 20:53:04 +0800

    代码规范

--- testFizzBuzz.py
+++ testFizzBuzz.py
@@ -1,16 +1,19 @@
 import unittest
 from FizzBuzz import FB
 
+
 class testFB(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(1,FB(1))
-    def test_3(self):
-        self.assertEqual("Fizz",FB(3))
-    def test_5(self):
-        self.assertEqual("Buzz",FB(5))
-    def test_15(self):
-        self.assertEqual("FizzBuzz",FB(15))
+        self.assertEqual(1, FB(1))
 
+    def test_3(self):
+        self.assertEqual("Fizz", FB(3))
+
+    def test_5(self):
+        self.assertEqual("Buzz", FB(5))
+
+    def test_15(self):
+        self.assertEqual("FizzBuzz", FB(15))
 
 
 if __name__ == "__main__":
