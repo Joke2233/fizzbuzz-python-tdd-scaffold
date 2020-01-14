412aa8d13b1239e57aaced6746d8a93d815de521
joke2233 <wanshuxiao@gmail.com>
2020-01-14 15:56:44 +0800

    三目运算符

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,10 +1,3 @@
 def FB(n):
-    if n % 3 == 0:
-        if n % 5 == 0:
-            return "FizzBuzz"
-        else:
-            return "Fizz"
-    elif n % 5 == 0:
-        return "Buzz"
-    else:
-        return n
+    r = "Fizz" if n % 3 == 0 else ''
+    return r + "Buzz" if n % 5 == 0 else n if r == '' else r
